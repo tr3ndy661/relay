7a90d52a941a38d287b1431fb46ee80d080b9038
Abdelmoneim <trendypotato661@gmail.com>
2026-09-05 21:42:43 +0300

    working on the models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Job (models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'pending'),
+        ('SUCCESS', 'success'),
+        ('FAILURE', 'failure'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+
+    # adding time stamps
